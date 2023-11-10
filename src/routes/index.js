a26638e85aeeb9498aead1499c3f7ac31fb06c3e
Miguel Palomera <miguel.palomera@gmail.com>
2023-11-10 16:31:37 +0000

    add websocket

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,19 @@ var admin = require("firebase-admin");
 var serviceAccount = require("../../serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://iot2023-5d518-default-rtdb.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://iot2023-5d518-default-rtdb.firebaseio.com"
 });
 
 const db = admin.database();
 
-const { Router}= require('express');
+const { Router, app } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
     db.ref('contacts').once('value', (snapshot) => {
-       data = snapshot.val();
-       res.render('index', {contacts: data})
+        data = snapshot.val();
+        res.render('index', { contacts: data })
     });
 })
 
@@ -34,5 +34,19 @@ router.get('/delete-contact/:id', (req, res) => {
     db.ref('contacts/' + req.params.id).remove();
     res.redirect('/');
 });
-
+// Esta es la parte que envia los datos a los clientes
+setInterval(() => {
+    aWss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
+}, 1000);
+// Deberian probar cambiarla a algo como
+/*
+db.ref('contacts').on('value', (snapshot) => {
+    data = snapshot.val();
+    aWss.clients.forEach((client) => {
+        client.send(data);
+    })
+});
+*/
 module.exports = router;
